56490793f47f7265a68f5a74f1d072023bf7a794
Abdul Kanjo <Abdul.kanjo@gmail.com>
2018-09-24 23:52:42 -0500

    Calculator done

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -10,55 +10,55 @@
     <div @click="sign" class="buttons">
       +/-
     </div>
-    <div class="buttons">
+    <div @click="percent" class="buttons">
       %
     </div>
-    <div class=" operator buttons">
+    <div @click="divide" class=" operator buttons">
       /
     </div>
-    <div class="buttons">
+    <div @click="append('7')" class="buttons">
       7
     </div>
-    <div class="buttons">
+    <div @click="append('8')" class="buttons">
      8
     </div>
-    <div class="buttons">
+    <div @click="append('9')" class="buttons">
      9
     </div>
-     <div class=" operator buttons">
+     <div @click="times" class=" operator buttons">
      x
     </div>
-     <div class="buttons">
+     <div @click="append('4')" class="buttons">
      4
     </div>
-     <div class="buttons">
+     <div @click="append('5')" class="buttons">
      5
     </div>
-     <div class="buttons">
+     <div @click="append('6')" class="buttons">
      6
     </div>
-     <div class=" operator buttons">
+     <div @click="minus" class=" operator buttons">
      -
     </div>
-     <div class="buttons">
+     <div @click="append('1')" class="buttons">
      1
     </div>
-     <div class="buttons">
+     <div @click="append('2')" class="buttons">
      2
     </div>
-     <div class="buttons">
+     <div @click="append('3')" class="buttons">
      3
     </div>
-     <div class=" operator buttons">
+     <div @click="add"  class=" operator buttons">
      +
     </div>
-     <div class="zero buttons">
+     <div @click="append('0')" class="zero buttons">
      0
     </div>
-     <div class="buttons">
+     <div @click="dot" class="buttons">
      .
     </div>
-     <div class=" operator buttons">
+     <div @click="equal" class=" operator buttons">
      =
     </div>
 
@@ -70,16 +70,62 @@
 export default {
   data() {
     return {
-      current: "1231"
+      current: "",
+      previous: null,
+      operator: null,
+      operatorClicked: false
     };
   },
   methods: {
     clear() {
       this.current = "";
+    },
+    sign() {
+      this.current =
+        this.current.charAt(0) === "-"
+          ? this.current.slice(1)
+          : `-${this.current}`;
+    },
+    percent() {
+      this.current = `${parseFloat(this.current) / 100}`;
+    },
+    append(number) {
+      if (this.operatorClicked) {
+        this.current = "";
+        this.operatorClicked = false;
+      }
+      this.current = `${this.current}${number}`;
+    },
+    dot() {
+      this.current.indexOf(".") === -1 ? this.append(".") : this.current;
+    },
+    setPreviouse() {
+      this.previous = this.current;
+      this.operatorClicked = true;
+    },
+    add() {
+      this.operator = (a, b) => a + b;
+      this.setPreviouse();
+    },
+    divide() {
+      this.operator = (a, b) => a / b;
+      this.setPreviouse();
+    },
+    minus() {
+      this.operator = (a, b) => a - b;
+      this.setPreviouse();
+    },
+    times() {
+      this.operator = (a, b) => a * b;
+      this.setPreviouse();
+    },
+    equal() {
+      this.current = `${this.operator(
+        parseFloat(this.current),
+        parseFloat(this.previous)
+      )}`;
+      this.previous = null;
     }
-  },
-  sign() {
-    this.current = this.current.charAt(0) === "-" ? this.current.slice(1) : ``;
   }
 };
 </script>
